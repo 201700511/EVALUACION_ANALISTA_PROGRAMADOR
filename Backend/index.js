a27821ff8ba57d817b199e7900a4464c1b564b40
jalvarado <3005421270101@ingenieria.usac.edu.gt>
2022-03-03 18:24:16 -0600

    Backbone: backend (+ modules)

---
+++ Backend/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+const cors = require('cors');
+var morgan = require("morgan");
+var jwt = require('jsonwebtoken');
+// const {JWK, JWE} = require('node-jose');
+
+
+//incriptation
+var app = express();
+var corsOptions = { origin: true, optionsSuccessStatus: 200 };
+app.use(morgan("combined"));
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: "10mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+
+
+//set headers enable
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS");
+    next();
+  });
+
+//settings
+const port = 9000;
+
+
+const rest = new (require('rest-mssql-nodejs'))({
+    user: 'semi2',
+    password: 'semi2',
+    server: 'LAPTOP-78OJDO4O',
+    database: 'USAC_EPS',
+    port: 1433,
+    options: { 
+        encrypt: true
+    } 
+});
+
+
+//imports routes
+const CustomerRoute = require ('./services/Customer/Customer')
+
+
+
+//routes or services
+app.use(CustomerRoute);
+
+
+app.listen(port, function () {
+  console.log("Service ESB ON: port ", port);
+});
